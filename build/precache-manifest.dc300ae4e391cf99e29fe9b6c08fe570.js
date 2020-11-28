033328af091d5475cd14bb5017a3e663a0191c09
vradu007 <radu.voin@gmail.com>
2020-11-28 22:51:17 +0200

    same as previous

---
+++ build/precache-manifest.dc300ae4e391cf99e29fe9b6c08fe570.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "994de4fe11e7af9591bae01527c9aa58",
+    "url": "/radu-shop/build/index.html"
+  },
+  {
+    "revision": "dd25ef0e305e8d7098fa",
+    "url": "/radu-shop/build/static/css/2.dec86d71.chunk.css"
+  },
+  {
+    "revision": "9bae9991a70147fdb793",
+    "url": "/radu-shop/build/static/css/main.4ba8e6d3.chunk.css"
+  },
+  {
+    "revision": "dd25ef0e305e8d7098fa",
+    "url": "/radu-shop/build/static/js/2.e951b5b8.chunk.js"
+  },
+  {
+    "revision": "7d2282234cd0247bd4712fb2834fc8d9",
+    "url": "/radu-shop/build/static/js/2.e951b5b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bae9991a70147fdb793",
+    "url": "/radu-shop/build/static/js/main.9e5da8be.chunk.js"
+  },
+  {
+    "revision": "b01aa582bc5bf2b467c0",
+    "url": "/radu-shop/build/static/js/runtime-main.6acf4f47.js"
+  },
+  {
+    "revision": "b41ee66a70d4a907834b2946742f3b8a",
+    "url": "/radu-shop/build/static/media/close.b41ee66a.svg"
+  },
+  {
+    "revision": "b2252809367a5a66646d1853d477c025",
+    "url": "/radu-shop/build/static/media/facebook.b2252809.svg"
+  },
+  {
+    "revision": "648cc3b4ec15006207a91af8a916e26d",
+    "url": "/radu-shop/build/static/media/github.648cc3b4.svg"
+  },
+  {
+    "revision": "81ad048ed858673aaca6cc2227076b8a",
+    "url": "/radu-shop/build/static/media/google.81ad048e.svg"
+  },
+  {
+    "revision": "d9b06de9c4288d366ab61c0dc60b357f",
+    "url": "/radu-shop/build/static/media/heart.d9b06de9.svg"
+  },
+  {
+    "revision": "173ca344d0b68783bf9f69f2e554e5cc",
+    "url": "/radu-shop/build/static/media/linkedin.173ca344.svg"
+  },
+  {
+    "revision": "c4d17416856a8e299c17aef22680b139",
+    "url": "/radu-shop/build/static/media/mail.c4d17416.svg"
+  },
+  {
+    "revision": "802cd03a0acab046d41deccc02c90a87",
+    "url": "/radu-shop/build/static/media/phone.802cd03a.svg"
+  },
+  {
+    "revision": "4eb6a9305e0b9b55be802ffa8bfc56c3",
+    "url": "/radu-shop/build/static/media/shopping-cart.4eb6a930.svg"
+  }
+]);
